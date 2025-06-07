84e834ee7d8d7cb069b7ebe88fad8c9f82e234d7
Wen Dong <wen.dong@microfocus.com>
2025-06-07 07:21:03 +0000

    t

--- terraform/modules/ec2/main.tf
+++ terraform/modules/ec2/main.tf
@@ -63,7 +63,6 @@ resource "aws_instance" "web" {
   subnet_id                   = var.subnet_id
   vpc_security_group_ids      = [aws_security_group.web_sg.id]
   iam_instance_profile        = aws_iam_instance_profile.ec2_profile.name
-  associate_public_ip_address = false
   key_name                    = var.key_name
 
   tags = {
